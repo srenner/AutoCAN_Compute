5b531e5974a12b27f3c7532b7d63479785925c27
Shawn Renner <shawn.renner@gmail.com>
2020-10-20 16:42:57 -0500

    screencap tests

--- code/test.py
+++ code/test.py
@@ -1,1 +1,14 @@
 # test3
+
+#import pyscreenshot as ImageGrab
+#im = ImageGrab.grab()
+#im.save("fullscreen.png")
+
+import cv2
+import time
+
+camera = cv2.VideoCapture(0)
+for i in range(100):
+	return_value, image = camera.read()
+	cv2.imwrite('screenshot'+str(i)+'.png', image)
+del(camera)
