d7c8f3b7ed728038a91dcc559ed6620884f3bc81
Shawn Renner <shawn.renner@gmail.com>
2020-10-22 10:30:38 -0500

    .bashrc snippet

--- code/wallpaper.sh
+++ code/wallpaper.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Run this on boot or login. Using .bashrc on the pi user should work.
+# Also makes sense to config your system to hide the cursor.
+
 fim -q /home/pi/AutoCAN_Imaging/wallpaper/logo.png
