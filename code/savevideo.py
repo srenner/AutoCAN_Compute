908cd869fafaf4127c5b78f643c5be73baa3f50e
Shawn Renner <shawn.renner@gmail.com>
2020-10-21 19:38:29 -0500

    mock up info bar

--- code/savevideo.py
+++ code/savevideo.py
@@ -1,16 +1,19 @@
 import cv2
 import time
+import numpy as np
 
 snapshot_interval = 1 #seconds
 snapshot_sequence = 1 #increment and use on filename
 do_snapshot = False
 
+#degree_sign= "\N{GREEK CAPITAL LETTER DELTA}"
+
 frame_count = 0
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-text_position = (2,12)
+text_position = (2,10)
 font_scale = .33
-font_color = (255,255,255)
+font_color = (0,0,0)
 line_type = 1
 
 video_cap = cv2.VideoCapture(0)
@@ -18,6 +21,13 @@ if (video_cap.isOpened() == False):
     print("Error opening video stream")
 frame_width = int(video_cap.get(3))
 frame_height = int(video_cap.get(4))
+
+
+#info bar rectangle
+x, y, w, h = 0, 0, frame_width, 15
+
+placeholder_text = '12/22/2020 13:25:24 (N) 43F (38.626550, -90.189260) 68MPH '
+
 size = (frame_width, frame_height)
 video_out = cv2.VideoWriter('../video/filename.mp4',
                          cv2.VideoWriter_fourcc(*'mp4v'),
@@ -30,7 +40,19 @@ try:
     while(True):
         ret, frame = video_cap.read()
         frame_count += 1
-        cv2.putText(frame,'Hello World ' + str(frame_count),
+
+
+        sub_img = frame[y:y+h, x:x+w]
+        white_rect = np.ones(sub_img.shape, dtype=np.uint8) * 255
+
+        res = cv2.addWeighted(sub_img, 0.5, white_rect, 0.5, 1.0)
+
+        # Putting the image back to its position
+        frame[y:y+h, x:x+w] = res
+
+
+
+        cv2.putText(frame, placeholder_text + str(frame_count),
             text_position,
             font,
             font_scale,
