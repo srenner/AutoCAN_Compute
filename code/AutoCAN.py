f59c52337bc223e875d2096de0356e1ec7708a14
Shawn Renner <shawn.renner@gmail.com>
2020-10-29 16:50:16 -0500

    start making multiprocess videos

--- code/AutoCAN.py
+++ code/AutoCAN.py
@@ -18,14 +18,96 @@ conn = None
 
 def create_video(camera_index, session_id, conn):
     session_active = 1
-    print("creating video - placeholder")
+    snapshot_interval = 1 #seconds
+    snapshot_sequence = 1 #increment and use on filename
+    frames_per_video = 1000
+    do_snapshot = False
+
+    frame_count = 0
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    text_position = (0,10)
+    font_scale = 0.8
+    font_color = (0,0,0)
+    line_type = 1
+
+    video_cap = cv2.VideoCapture(camera_index)
+    if (video_cap.isOpened() == False):
+        print("Error opening video stream")
+    frame_width = int(video_cap.get(3))
+    frame_height = int(video_cap.get(4))
+
+    #info bar rectangle
+    x, y, w, h = 0, 0, frame_width, 14
+    delimiter = '|'
+
+    can_data = { 'datetime': '10/25/2020 13:24:22',
+                'direction': 'NW',
+                'temp': '104F',
+                'lat_long': '(38.626550, -90.189260)',
+                'mph': '68MPH',
+                'g': '0.32 G',
+                'id': '104:325239'
+            }
+
+    placeholder_text = can_data['datetime'] +\
+                    delimiter +\
+                    can_data['direction'] +\
+                    delimiter +\
+                    can_data['temp'] +\
+                    delimiter +\
+                    can_data['lat_long'] +\
+                    delimiter +\
+                    can_data['mph'] +\
+                    delimiter +\
+                    can_data['g'] +\
+                    delimiter +\
+                    can_data['id'] +\
+                    delimiter
+
+    size = (frame_width, frame_height)
+    video_out = cv2.VideoWriter('../video/' + str(session_id) + '_camera_' + str(camera_index) + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 29.97, size)
+    do_continue = True
+    current_time = time.time()
+    next_snapshot_time = current_time + snapshot_interval
+
     c = conn.cursor()
     while(session_active):
-        #AND (sys_session_end IS NOT NULL OR gps_session_end IS NOT NULL)
         c.execute("SELECT COUNT(*) FROM session WHERE session_id = ? AND sys_session_end IS NULL AND gps_session_end IS NULL", (session_id,))
         result = c.fetchone()
         session_active = result[0]
-        print("camera " + str(camera_index) + " sees status of " + str(session_active))
+        if session_active:
+            ret, frame = video_cap.read()
+            #print(str(ret))
+            frame_count += 1
+
+            # put semi-transparent box on top of frame
+            sub_img = frame[y:y+h, x:x+w]
+            white_rect = np.ones(sub_img.shape, dtype=np.uint8) * 255
+            res = cv2.addWeighted(sub_img, 0.5, white_rect, 0.5, 1.0)
+            frame[y:y+h, x:x+w] = res
+
+            cv2.putText(frame, placeholder_text + str(frame_count),
+                text_position,
+                font,
+                font_scale,
+                font_color,
+                line_type)
+
+            do_continue = ret
+            if do_continue == True:
+                if do_snapshot == True:
+                    current_time = time.time()
+                    if current_time >= next_snapshot_time:
+                        next_snapshot_time = current_time + snapshot_interval
+                        cv2.imwrite('../snapshot/snapshot_' + camera_index + "_" + str(snapshot_sequence) + '.jpg', frame)
+                        snapshot_sequence += 1
+                video_out.write(frame)
+            else:
+                break
+
+    video_cap.release()
+    video_out.release()
     print("finishing up on camera " + str(camera_index))
 
 def create_session():
@@ -55,10 +137,10 @@ def main():
     GPIO.setup(17, GPIO.IN)
 
     if len(sys.argv) > 1:
-        front_camera_index = sys.argv[1]
+        front_camera_index = int(sys.argv[1])
         pass
     if len(sys.argv) > 2:
-        rear_camera_index = sys.argv[2]
+        rear_camera_index = int(sys.argv[2])
 
     if debug:
         print("Front camera index: " + str(front_camera_index))
@@ -69,14 +151,17 @@ def main():
     if debug:
         print("Session ID " + str(session_id))
 
-
     jobs = []
-    front_video = Process(target=create_video, args=(front_camera_index,session_id,conn))
-    rear_video = Process(target=create_video, args=(rear_camera_index,session_id,conn))
-    jobs.append(front_video)
-    jobs.append(rear_video)
-    front_video.start()
-    rear_video.start()
+
+    if int(front_camera_index) > -1:
+        front_video = Process(target=create_video, args=(front_camera_index,session_id,conn))
+        jobs.append(front_video)
+        front_video.start()
+
+    if int(rear_camera_index) > -1:
+        rear_video = Process(target=create_video, args=(rear_camera_index,session_id,conn))
+        jobs.append(rear_video)
+        rear_video.start()
 
     while power_on:
         try:
@@ -84,14 +169,19 @@ def main():
         except KeyboardInterrupt:
             if debug:
                 print("")
-                end_session()
                 print("Goodbye")
+            #end_session()
+            end_session()
+            conn.close()
             sys.exit()
 
     end_session()
     conn.close()
 
-
+    #if internet
+        #upload files and delete
+    #else
+        #save to subfolder
 
     time.sleep(5)
     power_on = not GPIO.input(17)
@@ -101,12 +191,6 @@ def main():
         if debug:
             print("Goodnight")
         #os.system("sudo shutdown -h now")  
-
-    #if internet
-        #upload files and delete
-    #else
-        #save to subfolder
-
 
 if __name__ == "__main__":
     main()
