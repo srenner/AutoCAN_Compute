7b8c38375235663e745b5bbeea1954505b9fcb2e
Shawn Renner <shawn.renner@gmail.com>
2020-11-18 08:39:31 -0600

    troubleshoot easycaps

--- code/AutoCAN.py
+++ code/AutoCAN.py
@@ -7,6 +7,8 @@ from multiprocessing import Process, Value
 import cv2
 import time
 import numpy as np
+import threading
+import faulthandler; faulthandler.enable()
 
 #power_on = 1
 power_on = True
@@ -34,10 +36,27 @@ def create_video(camera_index, session_id, conn_string):
         line_type = 1
 
         video_cap = cv2.VideoCapture(camera_index)
+        #video_cap.set(cv2.CV_CAP_PROP_CONVERT_RGB, False)
+        #video_cap.set(cv2.CV_CAP_PROP_MODE, cv2.CV_CAP_MODE_YUYV)
+        #video_cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
         if (video_cap.isOpened() == False):
             print("Error opening video stream")
         frame_width = int(video_cap.get(3))
         frame_height = int(video_cap.get(4))
+        frame_rate = float(video_cap.get(5))
+        #codec = str(video_cap.get(6))
+        #print(codec)
+
+        #print("LIST PROPS================")
+        #for x in range(21):
+        #    print(str(video_cap.get(x)))
+        #
+        #print("END PROPS=================")
+
+
+
+        #frame_width = 720
+        #frame_height = 480
 
         #info bar rectangle
         x, y, w, h = 0, 0, frame_width, 14
@@ -71,7 +90,8 @@ def create_video(camera_index, session_id, conn_string):
                         delimiter
 
         size = (frame_width, frame_height)
-        video_out = cv2.VideoWriter('../video/' + str(session_id) + '_camera_' + str(camera_index) + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 29.97, size)
+        #video_out = cv2.VideoWriter('../video/' + str(session_id) + '_camera_' + str(camera_index) + '.avi', cv2.VideoWriter_fourcc(*'YUYV'), frame_rate, size)
+        video_out = cv2.VideoWriter('../video/' + str(session_id) + '_camera_' + str(camera_index) + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), frame_rate, size)
         do_continue = True
         current_time = time.time()
         next_snapshot_time = current_time + snapshot_interval
@@ -167,11 +187,16 @@ def main():
         front_video = Process(target=create_video, args=(front_camera_index,session_id,conn_string))
         jobs.append(front_video)
         front_video.start()
+        #front_thread = threading.Thread(target=create_video, args=(front_camera_index,session_id,conn_string))
+        #front_thread.start()
+
 
     if int(rear_camera_index) > -1:
         rear_video = Process(target=create_video, args=(rear_camera_index,session_id,conn_string))
         jobs.append(rear_video)
         rear_video.start()
+        #rear_thread = threading.Thread(target=create_video, args=(rear_camera_index,session_id,conn_string))
+        #rear_thread.start()
 
     while power_on:
         try:
