70fa6f1f3db9b2f2fe64234301a94d46c37dfe97
Shawn Renner <shawn.renner@gmail.com>
2020-11-30 13:12:15 -0600

    placeholder to blank out screen

---
+++ code/blank.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run this on login. Using .bashrc on the pi user is one way.
+# Blinking cursor may be visible unless you config your system to remove it.
+
+fim -q /home/pi/AutoCAN_Imaging/wallpaper/blank.png
